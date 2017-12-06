68dabca84c808cc6535ea8954c6dfaaec9b4ba04
CROmarcos <marpavlov@foi.hr>
2017-12-06 13:06:42 +0100

    Update ograničenja

--- ZeroOneKnapsack/ZeroOneKnapsack/Form1.cs
+++ ZeroOneKnapsack/ZeroOneKnapsack/Form1.cs
@@ -43,27 +43,36 @@ namespace ZeroOneKnapsack
                 tbKapacitet.Visible = true;
             }
             else
-            {
-                btnDalje.Visible = false;
-                lbBrojVar.Visible = false;
-                lbKapacitet.Visible = false;
-                tbBrojVar.Visible = false;
-                tbKapacitet.Visible = false;
+            {                
+                if (tbBrojVar.Text == "0" || tbBrojVar.Text == "")
+                {
+                    MessageBox.Show("Molimo unesite broj varijabli!", "Upozorenje");
+                }
+                else if (tbKapacitet.Text == "0" || tbKapacitet.Text == "")
+                {
+                    MessageBox.Show("Molimo unesite kapacitet ranca!", "Upozorenje");
+                }
+                else
+                {
+                    btnDalje.Visible = false;
+                    lbBrojVar.Visible = false;
+                    lbKapacitet.Visible = false;
+                    tbBrojVar.Visible = false;
+                    tbKapacitet.Visible = false;
 
-                lbNetoVrijednost.Visible = true;
-                lbTezinskaVrijednost.Visible = true;
-                tbNetoVrijednost.Visible = true;
-                tbTezinskaVrijednost.Visible = true;
-                lbPreostalo.Visible = true;
+                    lbNetoVrijednost.Visible = true;
+                    lbTezinskaVrijednost.Visible = true;
+                    tbNetoVrijednost.Visible = true;
+                    tbTezinskaVrijednost.Visible = true;
+                    lbPreostalo.Visible = true;
 
-                btnUnesi.Visible = true;
+                    btnUnesi.Visible = true;
 
-                ranac.Kapacitet = Convert.ToInt32(tbKapacitet.Text);
-                brojVar = Convert.ToInt32(tbBrojVar.Text);
-                preostalo = brojVar;
-                lbPreostalo.Text = "Preostalo za unijeti: " + (preostalo).ToString();
-
-
+                    ranac.Kapacitet = Convert.ToInt32(tbKapacitet.Text);
+                    brojVar = Convert.ToInt32(tbBrojVar.Text);
+                    preostalo = brojVar;
+                    lbPreostalo.Text = "Preostalo za unijeti: " + (preostalo).ToString();
+                }
             }
         }
 
@@ -115,38 +124,49 @@ namespace ZeroOneKnapsack
 
         private void btnUnesi_Click(object sender, EventArgs e)
         {
-            if (brojac < brojVar)
+            if (tbNetoVrijednost.Text == "0" || tbNetoVrijednost.Text == "")
             {
-                Varijabla varijabla = new Varijabla();
-                varijabla.Naziv = "X" + (brojac + 1).ToString();
-                varijabla.NetoVrijednost = Convert.ToInt32(tbNetoVrijednost.Text);
-                varijabla.TezinskaVrijednost = Convert.ToInt32(tbTezinskaVrijednost.Text);
-                ranac.ListaUnosa.Add(varijabla);
-                brojac++;
-                preostalo--;
-                lbPreostalo.Text = "Preostalo za unijeti: " + (preostalo).ToString();
+                MessageBox.Show("Unesite neto vrijednost za varijablu!", "Upozorenje");
             }
-            if (brojac == brojVar)
+            else if (tbTezinskaVrijednost.Text == "0" || tbTezinskaVrijednost.Text == "")
             {
-                ranac.Sortiraj();
-                ranac.DodajVarijable();
+                MessageBox.Show("Unesite tezinsku vrijednost za varijablu!", "Upozorenje");
+            }
+            else
+            {
+                if (brojac < brojVar)
+                {
+                    Varijabla varijabla = new Varijabla();
+                    varijabla.Naziv = "X" + (brojac + 1).ToString();
+                    varijabla.NetoVrijednost = Convert.ToInt32(tbNetoVrijednost.Text);
+                    varijabla.TezinskaVrijednost = Convert.ToInt32(tbTezinskaVrijednost.Text);
+                    ranac.ListaUnosa.Add(varijabla);
+                    brojac++;
+                    preostalo--;
+                    lbPreostalo.Text = "Preostalo za unijeti: " + (preostalo).ToString();
+                }
+                if (brojac == brojVar)
+                {
+                    ranac.Sortiraj();
+                    ranac.DodajVarijable();
 
-                lbNetoVrijednost.Visible = false;
-                lbTezinskaVrijednost.Visible = false;
-                tbNetoVrijednost.Visible = false;
-                tbTezinskaVrijednost.Visible = false;
-                lbPreostalo.Visible = false;
-                btnUnesi.Visible = false;
-                btnPomoc.Visible = false;
-                btnOdustani.Visible = false;
+                    lbNetoVrijednost.Visible = false;
+                    lbTezinskaVrijednost.Visible = false;
+                    tbNetoVrijednost.Visible = false;
+                    tbTezinskaVrijednost.Visible = false;
+                    lbPreostalo.Visible = false;
+                    btnUnesi.Visible = false;
+                    btnPomoc.Visible = false;
+                    btnOdustani.Visible = false;
 
-                lbRjesenjeIznos.Text = ranac.IspisiIznos();
-                lbVarURancuIspis.Text = ranac.IspisiVarijable();
-                lbRjesenjeText.Visible = true;
-                lbRjesenjeIznos.Visible = true;
-                lbVarURancu.Visible = true;
-                lbVarURancuIspis.Visible = true;
-                btnPovratak.Visible = true;
+                    lbRjesenjeIznos.Text = ranac.IspisiIznos();
+                    lbVarURancuIspis.Text = ranac.IspisiVarijable();
+                    lbRjesenjeText.Visible = true;
+                    lbRjesenjeIznos.Visible = true;
+                    lbVarURancu.Visible = true;
+                    lbVarURancuIspis.Visible = true;
+                    btnPovratak.Visible = true;
+                }
             }
         }
 
